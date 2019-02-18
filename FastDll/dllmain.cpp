4567efa46a1ac9db85e8b2fa2584f80648c90dbd
denk <denkyuan@qq.com>
2019-02-18 15:06:02 +0800

    when/who call DllMain?

--- FastDll/dllmain.cpp
+++ FastDll/dllmain.cpp
@@ -1,5 +1,6 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "stdafx.h"
+#include<stdio.h>
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -9,9 +10,16 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+		printf("process attach of dll\n");
+		break;
     case DLL_THREAD_ATTACH:
+		printf("thread attach of dll\n");
+		break;
     case DLL_THREAD_DETACH:
+		printf("thread detach of dll\n");
+		break;
     case DLL_PROCESS_DETACH:
+		printf("process detach of dll\n");
         break;
     }
     return TRUE;
