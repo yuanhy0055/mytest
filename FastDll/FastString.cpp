3fd5c9e0d4de4bda7a7183a76cbc39631f669d83
denk <denkyuan@qq.com>
2019-02-19 11:51:08 +0800

    pfunc that caller can find it

--- FastDll/FastString.cpp
+++ FastDll/FastString.cpp
@@ -1,12 +1,33 @@
 ﻿#include "stdafx.h"
 #include "FastString.h"
-
+#include <stdio.h>
+#include <string.h>
 
 FastString::FastString(const char *psz)
 {
+	m_psz = new char[strlen(psz)+1];
+	strcpy(m_psz, psz);
 }
 
+extern "C" { __declspec(dllexport) int add(int x, int y); }
 
 FastString::~FastString()
 {
+	delete[] m_psz;
+}
+
+int FastString::Length() const
+{
+	return strlen(m_psz);
+}
+
+int FastString::Find(const char * psz) const
+{
+	printf("Func in progress...\n");
+	return 7;
+}
+
+int add(int x, int y)
+{
+	return x+y+10000;
 }
